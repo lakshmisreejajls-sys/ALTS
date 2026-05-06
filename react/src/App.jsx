cbb5bd85a82f4ff9cf5a650f394b2c0e8ab5d7f5
lakshmisreejajls-sys <lakshmisreeja.jls@gmail.com>
2026-05-06 12:22:25 +0530

    Add files via upload

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -4,29 +4,35 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [color, setColor] = useState("white");
+const App = () => {
+  // declaring state
+  const url =
+    'https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg'
 
-  const changeColor = () => {
-    const colors = ["#ffadad", "#caffbf", "#9bf6ff", "#ffd6a5", "#bdb2ff"];
-    const randomColor = colors[Math.floor(Math.random() * colors.length)];
-    setColor(randomColor);
-  };
+  const [image, setImage] = useState(url)
+
+  const changeAnimal = () => {
+    let dogURL =
+      'https://static.onecms.io/wp-content/uploads/sites/12/2015/04/dogs-pembroke-welsh-corgi-400x400.jpg'
+    let catURL =
+      'https://cdn.freecodecamp.org/curriculum/cat-photo-app/relaxing-cat.jpg'
+    let result = image === catURL ? dogURL : catURL
+    setImage(result)
+  }
 
   return (
-    <div
-      style={{
-        backgroundColor: color,
-        height: "100vh", 
-        textAlign: "center",
-        paddingTop: "100px",
-        transition: "0.5s",
-      }}
-    >
-      <h1>🎨 Color Changer App</h1>
-      <button onClick={changeColor}>Change Color</button>
+    <div className='App'>
+      <h1>Image Changes React</h1>
+      <div className='animal'>
+        <img src={image} alt='animal' />
+      </div>
+
+      <button onClick={changeAnimal} class='btn btn-add'>
+        Change
+      </button>
     </div>
-  );
+  )
 }
 
 export default App;
+
